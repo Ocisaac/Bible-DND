4141a4d810683a584f1a9980b1d170e6b3b9c5ed
Ocisaac <anivvt@gmail.com>
2017-02-05 22:14:06 +0200

    started work on utils

--- Utils.c
+++ Utils.c
@@ -2,12 +2,22 @@
 
 #include "Utils.h"
 
-void SetCursorPosition(int XPos, int YPos)
-{
+void SetCursorPosition(int XPos, int YPos){
     COORD Coord;
 
     Coord.X = XPos;
     Coord.Y = YPos;
 
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Coord);
+}
+
+//TODO:
+int wordCount(char* str){
+	
+
+}
+
+//TODO:
+void splitWords(char *str, char **words, int wordCount)
+{
 }
